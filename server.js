c9a7bd3170399cf14f6b3fce11201d4397b465d6
sarkar211997 <142913494+sarkar211997@users.noreply.github.com>
2026-02-09 00:16:14 +0000

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ let currentUser = null; // simple session
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://chizal461_db_user:wMalIj6FQKxKo7d2@cluster0.icfsvrw.mongodb.net/?appName=Cluster0")
+mongoose.connect("mongodb+srv://chizal461_db_user:<db_password>@cluster0.icfsvrw.mongodb.net/?appName=Cluster0")
   .then(() => console.log("Database Connected"));
 
 app.get("/", (req, res) => {
@@ -100,4 +100,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running");
 });
+
 
